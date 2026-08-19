18d03244012d7a397aec82a3a4fbf4d8cf833d23
marsap2003-hue <marsap2003@gmail.com>
2026-08-19 17:10:40 +0300

    Rename GazeboFollower to GazeboSlave and update logic

--- src/platooning_pkg/platooning_pkg/gazebo_slave.py
+++ src/platooning_pkg/platooning_pkg/gazebo_slave.py
@@ -6,32 +6,35 @@ from geometry_msgs.msg import Pose
 import subprocess
 
 
-class GazeboFollower(Node):
+class GazeboSlave(Node):
 
     def __init__(self):
-        super().__init__('gazebo_follower')
+        super().__init__('gazebo_slave')
+
         self.pose_publisher = self.create_publisher(
-    Pose,
-    '/follower_gazebo_pose',
-    10
-)
-        self.subscription = self.create_subscription(
             Pose,
-            '/leader_gazebo_pose',
-            self.leader_callback,
+            '/slave_gazebo_pose',
             10
         )
 
-        self.leader_x = 0.0
-        self.leader_y = 0.0
+        self.subscription = self.create_subscription(
+            Pose,
+            '/master_gazebo_pose',
+            self.master_callback,
+            10
+        )
 
-        self.follower_x = -8.0
-        self.follower_y = 0.0
+        self.master_x = 0.0
+        self.master_y = 0.0
+
+        self.slave_x = -8.0
+        self.slave_y = 0.0
         self.z = 0.5
 
         self.d0 = 5.0
         self.kp = 0.5
         self.dt = 0.1
+
         # Communication impairment parameters
         # Baseline configuration: no delay and no packet loss
         self.communication_delay = 0.0
@@ -42,46 +45,48 @@ class GazeboFollower(Node):
             self.update_position
         )
 
-def leader_callback(self, msg):
-time.sleep(self.communication_delay)
+    def master_callback(self, msg):
+        time.sleep(self.communication_delay)
 
-if random.random() < self.packet_loss_probability:
-    return
+        if random.random() < self.packet_loss_probability:
+            return
 
-    self.leader_x = msg.position.x
-    self.leader_y = msg.position.y
+        self.master_x = msg.position.x
+        self.master_y = msg.position.y
 
     def update_position(self):
 
-        desired_x = self.leader_x - self.d0
+        desired_x = self.master_x - self.d0
 
-        error = desired_x - self.follower_x
+        error = desired_x - self.slave_x
 
         velocity = self.kp * error
 
-        self.follower_x = self.follower_x + velocity * self.dt
+        self.slave_x = self.slave_x + velocity * self.dt
+
         pose_msg = Pose()
-        pose_msg.position.x = self.follower_x
-        pose_msg.position.y = self.follower_y
+        pose_msg.position.x = self.slave_x
+        pose_msg.position.y = self.slave_y
         pose_msg.position.z = self.z
         pose_msg.orientation.w = 1.0
 
         self.pose_publisher.publish(pose_msg)
+
         command = (
             'gz service -s /world/platoon_world/set_pose '
             '--reqtype gz.msgs.Pose '
             '--reptype gz.msgs.Boolean '
             '--timeout 1000 '
-            f'--req \'name: "follower_vehicle" '
-            f'position {{x: {self.follower_x} y: {self.follower_y} z: {self.z}}} '
+            f'--req \'name: "slave_vehicle" '
+            f'position {{x: {self.slave_x} y: {self.slave_y} z: {self.z}}} '
             'orientation {w: 1}\''
         )
 
         subprocess.run(command, shell=True)
 
         self.get_logger().info(
-            f'Leader x: {self.leader_x:.2f} | '
-            f'Follower x: {self.follower_x:.2f} | '
+            f'Master x: {self.master_x:.2f} | '
+            f'Slave x: {self.slave_x:.2f} | '
             f'Error: {error:.2f}'
         )
 
@@ -89,7 +94,7 @@ if random.random() < self.packet_loss_probability:
 def main(args=None):
     rclpy.init(args=args)
 
-    node = GazeboFollower()
+    node = GazeboSlave()
 
     rclpy.spin(node)
 
