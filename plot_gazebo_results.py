2e9b4c155fefb4fbce7eafc4c1ae54a511a76041
marsap2003-hue <marsap2003@gmail.com>
2026-08-19 17:16:51 +0300

    Refactor file paths for saving plots

--- plot_gazebo_results.py
+++ plot_gazebo_results.py
@@ -1,19 +1,17 @@
-import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data_file = os.path.join(os.getcwd(), 'gazebo_platooning_data.csv')
-data = pd.read_csv(data_file)
+data = pd.read_csv('gazebo_platooning_data.csv')
 
 plt.figure()
-plt.plot(data['time'], data['leader_position'], label='Leader Position')
-plt.plot(data['time'], data['follower_position'], label='Follower Position')
+plt.plot(data['time'], data['master_position'], label='Master Position')
+plt.plot(data['time'], data['slave_position'], label='Slave Position')
 plt.xlabel('Time (s)')
 plt.ylabel('Position (m)')
 plt.title('Gazebo Vehicle Positions')
 plt.grid(True)
 plt.legend()
-plt.savefig(os.path.join(os.getcwd(), 'gazebo_positions.png'), dpi=300)
+plt.savefig('gazebo_positions.png', dpi=300)
 
 plt.figure()
 plt.plot(data['time'], data['distance_error'], label='Distance Error')
@@ -22,6 +20,6 @@ plt.ylabel('Distance Error (m)')
 plt.title('Gazebo Distance Error')
 plt.grid(True)
 plt.legend()
-plt.savefig(os.path.join(os.getcwd(), 'gazebo_distance_error.png'), dpi=300)
+plt.savefig('gazebo_distance_error.png', dpi=300)
 
 plt.show()
