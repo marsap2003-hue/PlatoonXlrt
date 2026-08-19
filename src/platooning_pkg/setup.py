87e619e30b92df862c15983e663b98712ee94efb
marsap2003-hue <marsap2003@gmail.com>
2026-08-20 02:41:46 +0300

    Update package description for platooning package

--- src/platooning_pkg/setup.py
+++ src/platooning_pkg/setup.py
@@ -7,15 +7,20 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ms',
     maintainer_email='ms@todo.todo',
-    description='ROS2 package for vehicle platooning simulation',
+    description='ROS2 package for master-slave vehicle platooning simulation',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
