13c46e7e072658e16cbf32d54b07d61863d85948
marsap2003-hue <marsap2003@gmail.com>
2026-08-20 02:36:58 +0300

    Improve platoon_gazebo package metadata
    Updated package description for clarity and added missing newlines.

--- src/platoon_gazebo/setup.py
+++ src/platoon_gazebo/setup.py
@@ -9,17 +9,28 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-    ('share/ament_index/resource_index/packages',
-        ['resource/' + package_name]),
-    ('share/' + package_name, ['package.xml']),
-    (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-    (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.py')
+        ),
+        (
+            os.path.join('share', package_name, 'worlds'),
+            glob('worlds/*.sdf')
+        ),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ms',
     maintainer_email='ms@todo.todo',
-    description='TODO: Package description',
+    description='ROS2 package for Gazebo vehicle platooning simulation',
     license='TODO: License declaration',
     extras_require={
         'test': [
@@ -27,7 +38,6 @@ setup(
         ],
     },
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
