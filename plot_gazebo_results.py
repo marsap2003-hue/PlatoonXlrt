e36c99a357e3a51e16ece06b369856ee08630be8
marsap2003-hue <marsap2003@gmail.com>
2026-08-19 13:35:31 +0300

    Make Gazebo plotting paths portable
    Updated file paths to be relative to the current working directory.

--- plot_gazebo_results.py
+++ plot_gazebo_results.py
@@ -1,7 +1,9 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('/home/ms/ros2_ws/gazebo_platooning_data.csv')
+data_file = os.path.join(os.getcwd(), 'gazebo_platooning_data.csv')
+data = pd.read_csv(data_file)
 
 plt.figure()
 plt.plot(data['time'], data['leader_position'], label='Leader Position')
@@ -11,7 +13,7 @@ plt.ylabel('Position (m)')
 plt.title('Gazebo Vehicle Positions')
 plt.grid(True)
 plt.legend()
-plt.savefig('/home/ms/ros2_ws/gazebo_positions.png', dpi=300)
+plt.savefig(os.path.join(os.getcwd(), 'gazebo_positions.png'), dpi=300)
 
 plt.figure()
 plt.plot(data['time'], data['distance_error'], label='Distance Error')
@@ -20,6 +22,6 @@ plt.ylabel('Distance Error (m)')
 plt.title('Gazebo Distance Error')
 plt.grid(True)
 plt.legend()
-plt.savefig('/home/ms/ros2_ws/gazebo_distance_error.png', dpi=300)
+plt.savefig(os.path.join(os.getcwd(), 'gazebo_distance_error.png'), dpi=300)
 
 plt.show()
